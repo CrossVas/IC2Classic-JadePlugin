930b9b6bf4ebb58b364700dc78a8d285682c8bef
CrossVas <cross.wanted.1.4@gmail.com>
2025-02-22 10:31:37 +0900

    use slotCounts instead of fixed value

--- src/main/java/ic2/jadeplugin/providers/IndustrialWorkbenchInfo.java
+++ src/main/java/ic2/jadeplugin/providers/IndustrialWorkbenchInfo.java
@@ -4,12 +4,12 @@ import ic2.core.block.machines.logic.crafter.CraftRecipe;
 import ic2.core.block.machines.tiles.nv.IndustrialWorkbenchTileEntity;
 import ic2.jadeplugin.base.JadeHelper;
 import ic2.jadeplugin.base.interfaces.IInfoProvider;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.ChatFormatting;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class IndustrialWorkbenchInfo implements IInfoProvider {
@@ -19,8 +19,9 @@ public class IndustrialWorkbenchInfo implements IInfoProvider {
     @Override
     public void addInfo(JadeHelper helper, BlockEntity blockEntity, Player player) {
         if (blockEntity instanceof IndustrialWorkbenchTileEntity workbench) {
-            List<ItemStack> recipeOutputs = new ArrayList<>();
-            for (int i = 0; i < 27; i++) {
+            List<ItemStack> recipeOutputs = new ObjectArrayList<>();
+            int slots = workbench.recipes.getSlotCount();
+            for (int i = 0; i < slots; i++) {
                 CraftRecipe recipe = workbench.recipes.getRecipe(i);
                 if (recipe != null && recipe.isValid()) {
                     recipeOutputs.add(recipe.getDisplayItem());
@@ -30,6 +31,5 @@ public class IndustrialWorkbenchInfo implements IInfoProvider {
                 helper.addGrid(recipeOutputs, translate("ic2.probe.memory_expansion.can_craft.name").withStyle(ChatFormatting.YELLOW));
             }
         }
-
     }
 }
